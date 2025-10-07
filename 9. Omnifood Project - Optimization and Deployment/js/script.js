e11f64f744292b532cea13df5e5dae30e7a06c1f
Brenda Saucedo <brenda.s.g12@hotmail.com>
2025-10-07 00:45:02 -0500

    FINAL SECTION

--- 9. Omnifood Project - Optimization and Deployment/js/script.js
+++ 9. Omnifood Project - Optimization and Deployment/js/script.js
@@ -1,4 +1,94 @@
-///////////////////////////////////////////////////////////
+console.log("Hello, World!");
+
+const myName = "Bren";
+console.log(myName);
+
+const h1 = document.querySelector(".heading-primary");
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+// h1.textContent = myName;
+// h1.style.backgroundColor = "red";
+// h1.style.padding = "5rem";
+// });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentDate = new Date().getFullYear();
+yearEl.textContent = currentDate;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// No needed, since safari 2025 already include the smooth scrolling
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(link);
+
+    // Scroll back to top
+    if (href == "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if ((href !== "#") & href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+
+      // Close mobile navigation
+      if (link.classList.contains("main-nav-link")) {
+        headerEl.classList.toggle("nav-open");
+      }
+    }
+  });
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null, // viewport
+    threshold: 0, // trigger when 0% visible
+    rootMargin: "-80px", // offset = nav height
+  }
+);
+obs.observe(sectionHeroEl);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
@@ -17,56 +107,3 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
